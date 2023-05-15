46a2742cfbda6e782fe61aa968ae96983d2b46f0
Jarno Dijkmans <513745@student.fontys.nl>
2023-05-15 15:28:11 +0200

    test

---
+++ Application/GameLibrary/GameLibrary/obj/Release/net6.0/PubTmp/Out/wwwroot/js/detail.js
@@ -0,0 +1,26 @@
+﻿
+    document.addEventListener('DOMContentLoaded', function() {
+        var form = document.getElementById('addToCartForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+    var formData = new FormData(form);
+    fetch('', {
+        method: 'POST',
+    body: formData
+            })
+    .then(function(response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Error while adding item to the cart.');
+                }
+            })
+    .then(function(data) {
+        console.log("Cart item count: " + data.cartItemCount);
+                // Update the cart item count or show a success message
+            })
+    .catch(function(error) {
+        console.error(error);
+            });
+        });
+    });
--- Application/GameLibrary/GameLibrary/obj/Release/net6.0/PubTmp/Out/wwwroot/js/index.js
+++ Application/GameLibrary/GameLibrary/obj/Release/net6.0/PubTmp/Out/wwwroot/js/index.js
@@ -1,1 +1,11 @@
 ﻿
+    document.addEventListener('DOMContentLoaded', function() {
+    var carousel = document.getElementById('carouselsliderdemo');
+    var trailer = document.getElementById('game-trailer');
+
+    carousel.addEventListener('slid.bs.carousel', function() {
+        var src = trailer.src;
+    trailer.src = '';
+    trailer.src = src;
+    });
+});
